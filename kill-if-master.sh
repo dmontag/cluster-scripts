6ecf93e1b59a2c8319ffb70a4ac3b7b5a4e0ac57
David Montag <david.montag@neotechnology.com>
2011-11-17 18:01:55 -0800

    Added delay

--- kill-if-master.sh
+++ kill-if-master.sh
@@ -3,6 +3,7 @@
 SERVER_ID=`cat /opt/neo4j-enterprise-1.5/conf/neo4j.properties | grep server_id | tr '=' ' ' | awk '{print $2}'`
 
 if [ ! -z "`./status.rb | grep '$SERVER_ID @' | grep master`" ]; then 
+  sleep 10
   PID=`cat /opt/neo4j-enterprise-1.5/data/neo4j-service.pid`
   echo "Killing master process: $PID"
   kill -9 $PID
