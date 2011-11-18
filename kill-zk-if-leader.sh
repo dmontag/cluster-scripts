ee37ded6d9189416a8c526c47dc394f44945c47c
David Montag <david.montag@neotechnology.com>
2011-11-18 12:27:46 -0800

    Changed kill behavior

--- kill-zk-if-leader.sh
+++ kill-zk-if-leader.sh
@@ -2,16 +2,16 @@
 
 SERVER_ID=`cat /opt/neo4j-enterprise-1.5/conf/neo4j.properties | grep server_id | tr '=' ' ' | awk '{print $2}'`
 
-if [ ! "`./status.rb | grep -E '(leader|follower)' | wc -l`" -eq 3 ]; then 
+if [ ! "`./status.rb | grep -E '(leader|follower|unknown)' | wc -l`" -eq 3 ]; then 
   exit
 fi
 
 if [ ! -z "`./status.rb | grep "$SERVER_ID @" | grep leader`" ]; then 
   PID=`cat /opt/neo4j-enterprise-1.5/data/neo4j-coord.pid`
   echo "Killing ZK leader process: $PID (id=$SERVER_ID)"
-  sleep 10
+  sleep 2
   kill -9 $PID
-  if [ ! "`./status.rb | grep 'follower' | wc -l`" -eq 2 ]; then 
+  if [ ! "`./status.rb | grep -E '(leader|follower|unknown)' | wc -l`" -eq 3 ]; then
     exit
   fi
   sleep 1
