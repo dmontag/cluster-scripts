edd569f98e588e781a198ce649802797340cde40
David Montag <david.montag@neotechnology.com>
2011-11-17 17:59:07 -0800

    Removed manual server id config

--- kill-if-master.sh
+++ kill-if-master.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if [ ! -z "`./status.rb | grep '3 @' | grep master`" ]; then 
+SERVER_ID=`cat /opt/neo4j-enterprise-1.5/conf/neo4j.properties | grep server_id | tr '=' ' ' | awk '{print $2}'`
+
+if [ ! -z "`./status.rb | grep '$SERVER_ID @' | grep master`" ]; then 
   PID=`cat /opt/neo4j-enterprise-1.5/data/neo4j-service.pid`
   echo "Killing master process: $PID"
   kill -9 $PID
+  sleep 1
+  echo "Restarting Neo4j"
+  /opt/neo4j-enterprise-1.5/bin/neo4j start
 fi
 
